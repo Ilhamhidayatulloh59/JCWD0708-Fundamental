5855f550c8844ab6d448485bcb15b5d2f3789939
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2025-05-28 09:03:59 +0700

    intro programming & data types

---
+++ exercise/circle.ts
@@ -0,0 +1,6 @@
+const radius: number = 5;
+const diameter: number = radius * 2;
+const circumference: number = 2 * Math.PI * radius;
+const area = Math.PI * radius * radius;
+
+console.log(diameter, circumference, area);
---
+++ exercise/triangle.ts
@@ -0,0 +1,5 @@
+const a: number = 80;
+const b: number = 65;
+
+const c: number = 180 - (a + b);
+console.log(c);
