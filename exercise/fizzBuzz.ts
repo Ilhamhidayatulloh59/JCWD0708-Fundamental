ee6d29b0de88021a340ea7443ae747d19db88337
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2025-06-03 22:41:30 +0700

    exercise day-4

---
+++ exercise/fizzBuzz.ts
@@ -0,0 +1,16 @@
+function fizzBuzz(n: number) {
+  const res: (string | number)[] = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      res.push("FizzBuzz");
+    } else if (i % 3 == 0) {
+      res.push("Fizz");
+    } else if (i % 5 == 0) {
+      res.push("Buzz");
+    } else {
+      res.push(i);
+    }
+  }
+  return res.join(", ");
+}
+console.log(fizzBuzz(15));
---
+++ exercise/split.ts
@@ -0,0 +1,4 @@
+const myFunc = (str: string) => str.split(" ");
+
+console.log(myFunc("Hello World"));
+console.log(myFunc("Halo Purwadhika Bandung"));
